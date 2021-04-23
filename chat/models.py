94b2f66de0633328f096c141313241af5efb4a84
corutopi <39874652+corutopi@users.noreply.github.com>
2021-04-24 00:20:30 +0900

    [add] add models 'User' & 'AppToken'.

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class User(models.Model):
+    user_name = models.CharField(max_length=16, primary_key=True)
+    passwd = models.CharField(max_length=200)
+
+
+class AppToken(models.Model):
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    app_token = models.CharField(max_length=200)
+    ttl = models.DateTimeField('date published')
