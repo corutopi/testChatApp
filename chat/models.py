e0d39bbf97ca900ccbafd36bdced181035460100
corutopi <39874652+corutopi@users.noreply.github.com>
2021-04-28 02:03:06 +0900

    [update] change model column name.

--- chat/models.py
+++ chat/models.py
@@ -5,11 +5,11 @@ from django.db import models
 
 
 class User(models.Model):
-    user_name = models.CharField(max_length=16, primary_key=True)
-    passwd = models.CharField(max_length=200)
+    user_id = models.CharField(max_length=16, primary_key=True)
+    user_passwd = models.CharField(max_length=200)
 
 
 class AppToken(models.Model):
-    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     app_token = models.CharField(max_length=200)
     ttl = models.DateTimeField('date published')
